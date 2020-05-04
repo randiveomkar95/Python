ada6707c7927352d6d42026969ab48ab02953310
randiveomkar95 <randiveomkar95@gmail.com>
2020-05-04 11:25:29 +0530

    Reverse string problem

---
+++ problem1.py
@@ -0,0 +1,7 @@
+"""
+ Write a Python program which accepts the user's first and last name and
+ print them in reverse order with a space between them.
+"""
+firstname = input("Enter First name ")
+lastname = input("Enter Last name ")
+print(firstname[::-1], lastname[::-1])
