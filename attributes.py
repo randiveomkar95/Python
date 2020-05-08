15a276b6a88d49700d5575d69949353f2df6c258
randiveomkar95 <randiveomkar95@gmail.com>
2020-05-08 22:31:26 +0530

    Stack Problem

---
+++ attributes.py
@@ -0,0 +1,29 @@
+class Stack:
+    name = 0
+    numberList = []
+
+    def __init__(self, name):
+        self.name = name
+        Stack.numberList += 1
+
+        if Stack.numberList <= 5:
+            Stack.numberList.append(self)
+        else:
+            del Stack.numberList[0]
+            Stack.numberList.append(self)
+
+    @staticmethod
+    def displayStack():
+        for numbers in Stack.numberList:
+            print(numbers.name, end=' ')
+        print()
+
+
+numberOne = Stack("One")
+numberTwo = Stack("Two")
+numberThree = Stack("Three")
+numberFour = Stack("Four")
+numberFive = Stack("Five")
+numberFive.displayStack()
+numberSix = Stack("Six")
+numberSix.displayStack()
